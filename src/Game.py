11c27bff82f87943f556e68ad100d1643e938634
krasomil <krasomil@gmail.com>
2020-07-11 21:03:07 +0200

    fixed syntax errors, improved the game logic, added player container and name setters

--- src/Game.py
+++ src/Game.py
@@ -24,19 +24,20 @@ class Game():
         Initialize the game
         """
         self.logger.info("Initializing Database")
+        self.database = DatabaseHandler()
+        self.questionHandler = QuestionHandler()
         self.settingsHandler = SettingsHandler()
         self.settingsHandler.select_number_of_players()
         #create Computer instance (one computer always plays the game)
         self.computer = Computer()
         #create selected number of players
-        self.humanPlayers = [Human() for i in range(self.settingsHandler.number_players)] 
-
+        self.players = [Human() for i in range(self.settingsHandler.number_players)] 
         #set player names
-        
+        for i in range(len(self.players)):
+            self.players[i].set_name(i+1)
+        #append computer player to the list of all players
+        self.players.append(self.computer)
 
-
-        self.database = DatabaseHandler()
-        self.questionHandler = QuestionHandler()
         self.dice = Dice(6)
         self.logger.info("Polishing Dice")
         self.logger.info("Starting Game - Welcome to trivial purfuit")
@@ -49,24 +50,25 @@ class Game():
         """
         try:
             while(True):
-                #Human starts a game
-                self.logger.info("You start a game!")
-                # simulate roll
-                roll = self.dice.roll()
-                self.logger.info("You rolled a {0}".format(roll))
-                # simulate moving the piece
-                self.logger.info("Moving your piece...")
-                # simulate landing on a color
-                color = random.choice(self.colors)
-                self.logger.info("You landed on {0}".format(color))
-                self.logger.debug("Selecting {0} question".format(color))
-                # get a question from the color type
-                question = self.database.select_color_question(color)
-                # get an answer from the user
-                answer = self.questionHandler.query_user(question)
-                # evaluate the answer
-                self.questionHandler.evaluate(question, answer)
-                time.sleep(1)
+                for player in range(len(self.players)):
+                    #Human starts a game
+                    self.logger.info(self.players[player].name + " rolls a dice!")
+                    # simulate roll
+                    roll = self.dice.roll()
+                    self.logger.info(self.players[player].name + " rolled a {0}".format(roll))
+                    # simulate moving the piece
+                    self.logger.info("Moving the piece of " + self.players[player].name + "...")
+                    # simulate landing on a color
+                    color = random.choice(self.colors)
+                    self.logger.info(self.players[player].name +  " landed on {0}".format(color))
+                    self.logger.debug("Selecting {0} question".format(color))
+                    # get a question from the color type
+                    question = self.database.select_color_question(color)
+                    # get an answer from the user
+                    answer = self.questionHandler.query_user(question)
+                    # evaluate the answer
+                    self.questionHandler.evaluate(question, answer)
+                    time.sleep(1)
         except KeyboardInterrupt:
             sys.exit()
 
--- src/Player.py
+++ src/Player.py
@@ -2,13 +2,14 @@ import logging
 class Player():
     def __init__(self):
         self.name = ""
+        self.number = 0
         self.square_hub = []
         self.logger = logging.getLogger(__name__)
         logging.basicConfig(format='%(asctime)s - %(name)s: %(levelname)s - %(message)s')
 
-    def set_name(name, player_number):
+    def set_name(self, player_number):
         try:
-            self.logger.info("What's the name of player " + player_number + "?")
+            self.logger.info("What's the name of player " + str(player_number) + "?")
             self.name = str(input())
         except ValueError:
             self.name = ""
